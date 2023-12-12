a59bac6cdb39566538f783bde81c41c5df4f1e9d
bholmesdev <hey@bholmes.dev>
2023-12-11 23:12:37 -0500

    feat: wire up docsearch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,43 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlightDocSearch from "@astrojs/starlight-docsearch";
+import starlight from "@astrojs/starlight";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "ALGOLIA");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'Starwoof',
+      title: "Starwoof",
       tableOfContents: false,
-      social: {
-        github: 'https://github.com/withastro/starlight',
-      },
-      customCss: [
-        '@fontsource/atkinson-hyperlegible',
-        '/src/fonts.css'
+      plugins: [
+        starlightDocSearch({
+          appId: env.ALGOLIA_APP_ID,
+          apiKey: env.ALGOLIA_API_KEY,
+          indexName: env.ALGOLIA_INDEX_NAME,
+        }),
       ],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      customCss: ["@fontsource/atkinson-hyperlegible", "/src/fonts.css"],
       locales: {
         root: {
-          lang: 'en',
-          label: 'English',
+          lang: "en",
+          label: "English",
         },
         es: {
-          lang: 'es',
-          label: 'Español',
-        }
+          lang: "es",
+          label: "Español",
+        },
       },
       sidebar: [
         {
-          label: 'Pets',
-          autogenerate: { directory: 'pets' },
-        }
-      ]
+          label: "Pets",
+          autogenerate: { directory: "pets" },
+        },
+      ],
     }),
   ],
 });
